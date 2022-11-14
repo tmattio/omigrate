c69b6ef8afe4b4e8c518a3e98aa863de4c86b5d4
Etienne MARAIS <etienne@maiste.fr>
2022-11-14 12:35:20 +0100

    Conform examples to new tests

--- example.t/sqlite3/33_create_table.up.sql
+++ example.t/sqlite3/33_create_table.up.sql
@@ -1,3 +1,9 @@
 CREATE TABLE pets (
   name string NOT NULL DEFAULT "dog"
 );
+
+CREATE TABLE users (
+    name string NOT NULL DEFAULT "anonymous"
+);
+
+ALTER TABLE users ADD developer bool NOT NULL DEFAULT false;
